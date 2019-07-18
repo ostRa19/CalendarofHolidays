76ee05ee22ecf8e00c0d692c86ed39797d660532
“Ostapenko <radraf16@gmail.com>
2019-07-18 14:00:46 +0300

    refactoring

--- app/Console/Commands/PrivatData.php
+++ app/Console/Commands/PrivatData.php
@@ -13,14 +13,14 @@ class PrivatData extends Command
      *
      * @var string
      */
-    protected $signature = 'command:get_exchange_rates';
+    protected $signature = 'command:currency';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Get exchange rates';
 
     /**
      * Create a new command instance.
@@ -39,58 +39,48 @@ class PrivatData extends Command
      */
     public function handle()
     {
-    try{
-        $cash_json_string = file_get_contents('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
-        $cash_json_array = json_decode($cash_json_string, TRUE);
-        // echo'<br>cash';  // check required data
-        // echo'<br>'.$cash_json_array[0]['ccy'].'<br>'.$cash_json_array[0]['base_ccy'].'<br>'.$cash_json_array[0]['buy'].'<br>'.$cash_json_array[0]['sale'].'<br>';
-        // echo'<br>'.$cash_json_array[1]['ccy'].'<br>'.$cash_json_array[1]['base_ccy'].'<br>'.$cash_json_array[1]['buy'].'<br>'.$cash_json_array[1]['sale'].'<br>';
-
-        $cashless_json_string = file_get_contents('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11');
-        $cashless_json_array = json_decode($cashless_json_string, TRUE);
-        // echo'<br>cashless'; //check required data
-        // echo'<br>'.$cashless_json_array[0]['ccy'].'<br>'.$cashless_json_array[0]['base_ccy'].'<br>'.$cashless_json_array[0]['buy'].'<br>'.$cashless_json_array[0]['sale'].'<br>';
-        // echo'<br>'.$cashless_json_array[1]['ccy'].'<br>'.$cashless_json_array[1]['base_ccy'].'<br>'.$cashless_json_array[1]['buy'].'<br>'.$cashless_json_array[1]['sale'].'<br>';
-    }
-    catch(Exception $e){
-        echo 'Exception is thrown: ',  $e->getMessage(), "\n";
-    }
+        try{
+            $cash_json_string = file_get_contents(env('CASH_URL'));
+            $cash_json_array = json_decode($cash_json_string, TRUE);
+            
+            $cashless_json_string = file_get_contents(env('CASHLESS_URL'));
+            $cashless_json_array = json_decode($cashless_json_string, TRUE);
+            
+        }
+        catch(Exception $e){
+            echo 'Exception is thrown: ',  $e->getMessage(), "\n";
+        }
 
         $curDate = date('Y-m-d h:i:s', time());
        
-try{
-    DB::table('privatdata')->insert([
-        'ccy_cash_USD' => $cash_json_array[0]['ccy'],
-        'base_ccy_cash_USD' => $cash_json_array[0]['base_ccy'],
-        'buy_cash_USD' => $cash_json_array[0]['buy'],
-        'sale_cash_USD' => $cash_json_array[0]['sale'],
-        
-        'ccy_cash_EUR' => $cash_json_array[1]['ccy'],
-        'base_ccy_cash_EUR' => $cash_json_array[1]['base_ccy'],
-        'buy_cash_EUR' => $cash_json_array[1]['buy'],
-        'sale_cash_EUR' => $cash_json_array[1]['sale'],
-        
-        'ccy_USD' => $cashless_json_array[0]['ccy'],
-        'base_ccy_USD' => $cash_json_array[0]['base_ccy'],
-        'buy_USD' => $cashless_json_array[0]['buy'],
-        'sale_USD' => $cashless_json_array[0]['sale'],
+        try{
+            DB::table('privatdata')->insert([
+                'ccy_cash_USD' => $cash_json_array[0]['ccy'],
+                'base_ccy_cash_USD' => $cash_json_array[0]['base_ccy'],
+                'buy_cash_USD' => $cash_json_array[0]['buy'],
+                'sale_cash_USD' => $cash_json_array[0]['sale'],
+                
+                'ccy_cash_EUR' => $cash_json_array[1]['ccy'],
+                'base_ccy_cash_EUR' => $cash_json_array[1]['base_ccy'],
+                'buy_cash_EUR' => $cash_json_array[1]['buy'],
+                'sale_cash_EUR' => $cash_json_array[1]['sale'],
+                
+                'ccy_USD' => $cashless_json_array[0]['ccy'],
+                'base_ccy_USD' => $cash_json_array[0]['base_ccy'],
+                'buy_USD' => $cashless_json_array[0]['buy'],
+                'sale_USD' => $cashless_json_array[0]['sale'],
 
-        'ccy_EUR' => $cashless_json_array[1]['ccy'],
-        'base_ccy_EUR' => $cashless_json_array[1]['base_ccy'],
-        'buy_EUR' => $cashless_json_array[1]['buy'],
-        'sale_EUR' => $cashless_json_array[1]['sale'],
+                'ccy_EUR' => $cashless_json_array[1]['ccy'],
+                'base_ccy_EUR' => $cashless_json_array[1]['base_ccy'],
+                'buy_EUR' => $cashless_json_array[1]['buy'],
+                'sale_EUR' => $cashless_json_array[1]['sale'],
 
-        'created_at' => $curDate,
-        'updated_at' => $curDate
-    ]);
-}
-catch(Exception $e){
-    echo 'Exception is thrown: ',  $e->getMessage(), "\n";
-}
-
-    // $all_rates_now = DB::table('privatdata')->orderBy('id', 'DESC')->first();  
-    // $this->line('Exchange Rates now:');
-    //dd($all_rates_now);
-
+                'created_at' => $curDate,
+                'updated_at' => $curDate
+            ]);
+        }
+        catch(Exception $e){
+            echo 'Exception is thrown: ',  $e->getMessage(), "\n";
+        }
     }
 }    
